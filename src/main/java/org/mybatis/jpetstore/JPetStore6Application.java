3165df5f61a775ca2ae55da2f198a1b29ce07cdd
Igor Baiborodine <ibaiborodine@gmail.com>
2015-09-17 13:07:33 -0400

    Migrate to Spring Boot/Java config and Java 8

--- src/main/java/org/mybatis/jpetstore/JPetStore6Application.java
+++ src/main/java/org/mybatis/jpetstore/JPetStore6Application.java
@@ -1,4 +1,21 @@
+/**
+ *    Copyright 2010-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.mybatis.jpetstore;
+
+import static javax.servlet.DispatcherType.REQUEST;
 
 import net.sourceforge.stripes.controller.StripesFilter;
 
@@ -16,13 +33,11 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import static javax.servlet.DispatcherType.REQUEST;
-
 @SpringBootApplication
 public class JPetStore6Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
-    SpringApplication.run(new Class[] {JPetStoreDemo6SpringBootApplication.class}, args);
+    SpringApplication.run(new Class[]{JPetStore6Application.class}, args);
   }
 
   @Override
@@ -51,5 +66,4 @@ public class JPetStore6Application extends SpringBootServletInitializer {
     stripesDispatcher.addMapping("*.action");
     super.onStartup(servletContext);
   }
-
 }
