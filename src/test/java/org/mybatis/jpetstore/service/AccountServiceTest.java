faf4e7d9629ab545abbbd250fbad9a3e126239f1
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-02-14 19:02:46 +0100

    Some extra simple tests

---
+++ src/test/java/org/mybatis/jpetstore/service/AccountServiceTest.java
@@ -0,0 +1,55 @@
+/**
+ *    Copyright 2010-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.jpetstore.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mybatis.jpetstore.domain.Account;
+import org.mybatis.jpetstore.persistence.AccountMapper;
+
+/**
+ * @author Eduardo Macarron
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AccountServiceTest {
+
+  @Mock
+  private AccountMapper accountMapper;
+  
+  @InjectMocks
+  private AccountService accountService;
+
+  @Test
+  public void shouldCallTheMapperToInsertAnAccount() {
+    //given
+    Account account = new Account();
+
+    //when
+    accountService.insertAccount(account);
+    
+    //then
+    Mockito.verify(accountMapper).insertAccount(Matchers.eq(account));
+    Mockito.verify(accountMapper).insertProfile(Matchers.eq(account));
+    Mockito.verify(accountMapper).insertSignon(Matchers.eq(account));
+  }
+
+}
---
+++ src/test/java/org/mybatis/jpetstore/service/CatalogServiceTest.java
@@ -0,0 +1,72 @@
+/**
+ *    Copyright 2010-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.jpetstore.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mybatis.jpetstore.domain.Product;
+import org.mybatis.jpetstore.persistence.CategoryMapper;
+import org.mybatis.jpetstore.persistence.ItemMapper;
+import org.mybatis.jpetstore.persistence.ProductMapper;
+
+/**
+ * @author Eduardo Macarron
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CatalogServiceTest {
+
+  @Mock
+  private CategoryMapper categoryMapper;
+  @Mock
+  private ItemMapper itemMapper;
+  @Mock
+  private ProductMapper productMapper;
+
+  @InjectMocks
+  private CatalogService catalogService;
+
+  @Test
+  public void shouldCallTheSearchMapperTwice() {
+    //given
+    String keywords = "a b";
+    List<Product> l1 = new ArrayList<Product>();
+    l1.add(new Product());
+    List<Product> l2 = new ArrayList<Product>();
+    l2.add(new Product());
+    
+    //when
+    when(productMapper.searchProductList("%a%")).thenReturn(l1);
+    when(productMapper.searchProductList("%b%")).thenReturn(l2);
+    
+    //then
+    List<Product> r = catalogService.searchProductList(keywords);
+    assertEquals(2, r.size());
+    assertSame(r.get(0), l1.get(0));
+    assertSame(r.get(1), l2.get(0));
+  }
+
+}
