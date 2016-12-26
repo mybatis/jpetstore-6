98c0fc29846fbb186bb0057c9dd49df217f1849f
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-12-26 09:26:17 +0900

    Fix to no use deprecated api fix gh-60

--- src/test/java/org/mybatis/jpetstore/service/OrderServiceTest.java
+++ src/test/java/org/mybatis/jpetstore/service/OrderServiceTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mybatis.jpetstore.domain.Item;
 import org.mybatis.jpetstore.domain.LineItem;
 import org.mybatis.jpetstore.domain.Order;
