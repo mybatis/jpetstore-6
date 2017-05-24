f71c29bbebf1b4798119beaab5d8018676ca1f00
Jeremy Landis <jeremylandis@hotmail.com>
2017-05-23 22:33:26 -0400

    [ci] Correct travis after script

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -31,9 +31,9 @@ echo "Current commit detected: ${commit_message}"
 # 4. Notify Coveralls.
 # 5. Deploy site
 
-if [ "$mybatis_repo" == "https://github.com/mybatis/jpetstore-6.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
+if [ $TRAVIS_REPO_SLUG == "mybatis/jpetstore-6" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
 
-  if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ]; then
+  if [ ${TRAVIS_JDK_VERSION} == "oraclejdk8" ]; then
 
     # Deploy to sonatype
     ./mvnw clean deploy -q --settings ./travis/settings.xml
