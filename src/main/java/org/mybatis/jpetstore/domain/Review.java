ae76c32bf79b58c691b704c74a491c8fa331ea6c
Tekiter <tekiter8@gmail.com>
2022-11-21 14:51:27 +0900

    feat: 리뷰 관련 클래스들 추가

---
+++ src/main/java/org/mybatis/jpetstore/domain/Review.java
@@ -0,0 +1,71 @@
+package org.mybatis.jpetstore.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Review implements Serializable {
+
+    private String reviewId;
+    private String itemId;
+    private String userId;
+    private String content;
+    private int rating;
+    private String pictureUrl;
+    private Date createdAt;
+
+    public String getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(String reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
