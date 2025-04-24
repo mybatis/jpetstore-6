9ef948bf3589f883018580f2e8963b8019196ede
yyy <yyy@gmail.com>
2025-04-24 17:12:08 +0800

    add non functional method in product.java

--- src/main/java/org/mybatis/jpetstore/domain/Product.java
+++ src/main/java/org/mybatis/jpetstore/domain/Product.java
@@ -68,4 +68,73 @@ public class Product implements Serializable {
     return getName();
   }
 
+  package org.mybatis.jpetstore.domain;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+/**
+ * The Class Product.
+ *
+ * @author Eduardo Macarron
+ */
+public class Product implements Serializable {
+
+  private static final long serialVersionUID = -7492639752670189553L;
+  private static final Logger LOGGER = Logger.getLogger(Product.class.getName());
+
+  private String productId;
+  private String categoryId;
+  private String name;
+  private String description;
+
+  public String getProductId() {
+    return productId;
+  }
+
+  public void setProductId(String productId) {
+    this.productId = productId.trim();
+  }
+
+  public String getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(String categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return getName();
+  }
+
+  // --- Non-functional method added for demonstration ---
+  public void printDebugInfo() {
+    LOGGER.info("Debug Product Info: " + this.toString());
+  }
+
+  public boolean isEmpty() {
+    return (productId == null || productId.isEmpty()) &&
+           (name == null || name.isEmpty());
+  }
+}
+
+
 }
