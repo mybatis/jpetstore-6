a6b20f43809f15db0ad27bdda93c9e4efbb27d86
Jeremy Landis <jeremylandis@hotmail.com>
2026-08-09 20:25:36 -0400

    Avoid star imports

--- src/test/java/org/mybatis/jpetstore/service/OrderServiceTest.java
+++ src/test/java/org/mybatis/jpetstore/service/OrderServiceTest.java
@@ -17,7 +17,9 @@ package org.mybatis.jpetstore.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
